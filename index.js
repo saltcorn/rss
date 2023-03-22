e5a24833e756bb2216a7fd75d79324276e9242c3
Tom Nielsen <tanielsen@gmail.com>
2023-03-22 17:37:39 +0000

    basic structure

---
+++ index.js
@@ -0,0 +1,8 @@
+const { features } = require("@saltcorn/data/db/state");
+const Workflow = require("@saltcorn/data/models/workflow");
+const Form = require("@saltcorn/data/models/form");
+
+module.exports = {
+  sc_plugin_api_version: 1,
+  plugin_name: "rss",
+};
