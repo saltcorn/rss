b4bc427df081c977c9de9507d49ef5cc108658e0
Tom Nielsen <tanielsen@gmail.com>
2023-03-23 13:23:22 +0000

    new fields location; comments

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const configuration_workflow = () =>
 
 const cache = {};
 
+//cache becomes stale after for 5 mins
 const stale = (t) => {
   const diff_ms = new Date().getTime() - t.getTime();
   return diff_ms > 5 * 60 * 1000;
@@ -38,21 +39,28 @@ module.exports = {
   table_providers: {
     "RSS feed": {
       configuration_workflow,
+      fields: [
+        { name: "title", label: "Title", type: "String" },
+        { name: "link", label: "Link", type: "String" },
+      ],
       get_table: (cfg) => {
         return {
           getRows: async () => {
+            //may not beconfigured yet
             if (!cfg?.url) return [];
+
+            // look up in cache
             if (cache[cfg.url] && !stale(cache[cfg.url].time))
               return cache[cfg.url].items;
+
+            //get RSS items
             const parser = new Parser();
-            const rows = await parser.parseURL(cfg.url);
-            cache[cfg.url] = { time: new Date(), items: rows.items };
-            return rows.items;
+            const feed = await parser.parseURL(cfg.url);
+
+            //store in cache
+            cache[cfg.url] = { time: new Date(), items: feed.items };
+            return feed.items;
           },
-          fields: [
-            { name: "title", label: "Title", type: "String" },
-            { name: "link", label: "Link", type: "String" },
-          ],
         };
       },
     },
